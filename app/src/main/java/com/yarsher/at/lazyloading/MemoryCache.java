6f397670dbb55bdeeadb4f6fff7e253f76fbf6d9
Yar <yarsher.dev@gmail.com>
2019-04-04 14:04:07 -0400

    checkSize

--- app/src/main/java/com/yarsher/at/lazyloading/MemoryCache.java
+++ app/src/main/java/com/yarsher/at/lazyloading/MemoryCache.java
@@ -3,12 +3,13 @@ package com.yarsher.at.lazyloading;
 import android.graphics.Bitmap;
 
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MemoryCache {
 
-    private Map<String, Bitmap> chache = Collections.synchronizedMap(
+    private Map<String, Bitmap> cache = Collections.synchronizedMap(
             //Last argument true for LRU ordering
             new LinkedHashMap<String, Bitmap>(10, 0.75f, true)
     );
@@ -22,6 +23,48 @@ public class MemoryCache {
 
     public void setLimit(long newLimit){
         limit = newLimit;
+    }
+
+    public Bitmap get(String id){
+        try{
+            if (!cache.containsKey(id)) return null;
+            return cache.get(id);
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void put(String id, Bitmap bitmap){
+        try {
+            if (cache.containsKey(id)) size -= getSizeInBytes(cache.get(id));
+            cache.put(id, bitmap);
+            size += getSizeInBytes(bitmap);
+            checkSize();
+        }catch (Throwable e){
+            e.printStackTrace();
+        }
+    }
+
+    private void checkSize() {
+        if (size>limit){
+            //least recently accessed item will be the first one iterated
+            Iterator<Map.Entry<String, Bitmap>> iterator = cache.entrySet().iterator();
+            while (iterator.hasNext()){
+                Map.Entry<String, Bitmap> entry = iterator.next();
+                size -= getSizeInBytes(entry.getValue());
+                iterator.remove();
+                if (size<=limit) break;
+            }
+        }
+    }
+
+
+
+
+    long getSizeInBytes(Bitmap bitmap){
+        if (bitmap == null) return 0;
+        return bitmap.getRowBytes() * bitmap.getHeight();
     }
 
 
