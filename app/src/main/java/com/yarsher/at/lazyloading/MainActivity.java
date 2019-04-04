f944c7f6f96dc209135f77cdcfd829ce390c6ff8
Yar <yarsher.dev@gmail.com>
2019-04-04 13:44:28 -0400

    init

--- app/src/main/java/com/yarsher/at/lazyloading/MainActivity.java
+++ app/src/main/java/com/yarsher/at/lazyloading/MainActivity.java
@@ -1,13 +1,28 @@
 package com.yarsher.at.lazyloading;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public class MemoryCache{
+        private static final String TAG = "MemoryCache";
+        private Map<String, Bitmap> cache = Collections.synchronizedMap(new LinkedHashMap<String, Bitmap>(){
+        //   setLimit(Runtime.getRuntime().maxMemory() / 4);
+        });
     }
 }
